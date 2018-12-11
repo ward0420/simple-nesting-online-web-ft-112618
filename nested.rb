37b840ece3717fbcbc55cf1a432a6105c10dcce9
Don Ward <10is0420@gmail.com>
2018-12-11 12:42:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -80,7 +86,9 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
+  
 end
 
 def changing_alan
@@ -102,7 +110,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
+  
 end
 
 def adding_to_dennis
@@ -121,5 +132,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 
 end
